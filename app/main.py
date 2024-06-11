c1cedc4edba73abe5f61b40a7ce55a36e7692b40
YongERong <yong_e-rong@s2017.sst.edu.sg>
2024-06-11 18:27:28 +0800

    feat: change btn colour when clicked

--- app/main.py
+++ app/main.py
@@ -1,25 +1,23 @@
 import streamlit as st
 
 # Get emojis
-n = 5
 emojis = ["😀", "😆", "😂", "🤨", "🤓"]
-
+n = len(emojis)
 
 # Setup state
 if 'is_pressed' not in st.session_state:
-    st.session_state.is_pressed = ["secondary"] * n
+    st.session_state.is_pressed = [False] * n
+
+def setState(key):
+    st.session_state.is_pressed[key] = False if st.session_state.is_pressed[key] else True
 
 # Buttons
 cols = st.columns(n)
-def setState(key):
-    st.session_state.is_pressed[key] = "primary" if st.session_state.is_pressed[key] == "secondary" else "secondary"
-    print(st.session_state.is_pressed)
 
 for i in range(n):
     col = cols[i]
     with col:
-        st.button(emojis[i], use_container_width=True, type=st.session_state.is_pressed[i], args=i)
+        st.button(label=f":{"red" if st.session_state.is_pressed[i] == True else "black"}-background[{emojis[i]}]", use_container_width=True, on_click=setState, kwargs={"key":i}, key=i)
 
-# TODO: On click
 
 st.button("GOO")
